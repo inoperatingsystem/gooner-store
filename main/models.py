54f44e01db456a7466dd5249830b5f02ae9053c7
inoperatingsystem <julius.albert@cs.ui.ac.id>
2025-09-17 09:08:23 +0700

    progress tugas-3

--- main/models.py
+++ main/models.py
@@ -11,6 +11,14 @@ class Product(models.Model):
         ('accessory', 'Accessory'),
     ]
 
+    CATEGORY_SIZES = [
+        ('xs', 'XS'),
+        ('s', 'S'),
+        ('m', 'M'),
+        ('l', 'L'),
+        ('xl', 'XL'),
+    ]
+
     # Mandatory attributes
     name = models.CharField(max_length=256)
     price = models.IntegerField()
@@ -22,6 +30,38 @@ class Product(models.Model):
     # Additional attributes
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     stock = models.IntegerField()
+    size = models.CharField(max_length=20, choices=CATEGORY_SIZES)
+    date_added = models.DateTimeField(auto_now_add=True)
+    sold_count = models.IntegerField(default=0)
+    is_available = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
+    
+    @property
+    def is_in_stock(self):
+        return self.stock > 0
+    
+    def reduce_stock(self, quantity=1):
+        if quantity <= 0:
+            return False
+        if quantity > self.stock:
+            return False
+        self.stock -= quantity
+        self.save()
+        return True
+    
+    def add_stock(self, quantity):
+        if quantity <= 0:
+            return False
+        self.stock += quantity
+        self.save()
+        return True
+    
+    def increment_sold_count(self, quantity=1):
+        self.sold_count += quantity
+        self.save()
+
+    @property
+    def is_best_selling(self):
+        return self.sold_count > 20
--- main/views.py
+++ main/views.py
@@ -1,11 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from main.forms import ProductForm
+from main.models import Product
 
 # Create your views here.
 def show_main(request):
+    product_list = Product.objects.all()
+
     context = {
         'app_name' : 'Gooner Store',
         'name' : 'Julius Albert Wirayuda',
         'class' : 'PBP F',
+        'product_list': product_list,
     }
 
     return render(request, "main.html", context)
+
+def add_product(request):
+    form = ProductForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:show_main')
+    
+    context = {'form': form}
+    return render(request, "add_product.html", context)
+
+def show_product(request, id):
+    product = get_object_or_404(Product, pk=id)
+
+    context = {
+        'product': product
+    }
+
+    return render(request, "product_detail.html", context)
